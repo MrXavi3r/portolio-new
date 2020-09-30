920195394e466ea262dd1e5e229542d7e65b6918
Xavier Ritch <xritch89@hotmail.com>
2020-09-30 16:50:07 -0400

    fixed workInquiry component layout

--- src/components/WorkInquiry.js
+++ src/components/WorkInquiry.js
@@ -5,25 +5,38 @@ import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
 
 export default function WorkInquiry() {
   return (
-    <Card className="container-md mt-5 mr-5 py-3" border="secondary">
-     <Card className="w-25 rounded-circle" bg="info">
-        <FontAwesomeIcon icon={faBriefcase} size="3x" className="mx-auto" />
-        </Card>
-      <Card.Body>
-        <Container>
-          <Row>
-            <Col>
-              <Card.Title>Work Inquiry</Card.Title>
+    <Container>
+      <Card
+        className="container-md mt-5 mr-5 py-3"
+        border="secondary"
+        bg="warning"
+      >
+        <Row>
+          <Col>
+          <Card bg="warning" border="warning">
+          <Card className="w-25 rounded-circle" bg="info">
+              <FontAwesomeIcon
+                icon={faBriefcase}
+                size="3x"
+                className="m-auto"
+                color="brown"
+              />
+            </Card>
+            <Card.Title className="text-center">Work Inquiry</Card.Title>
+            <Card.Body>
               <Card.Text>
-                Incredible opportunities are all around. Let's work together
-              </Card.Text>
-            </Col>
-            <Col className="d-flex justify-content-center">
-            <Button variant="primary" type="button" size="lg" className="h-75 mx-auto">CONNECT</Button>
-            </Col>
-          </Row>
-        </Container>
-      </Card.Body>
-    </Card>
+                  Incredible opportunities are all around. Let's work together
+                </Card.Text>
+            </Card.Body>
+            </Card>
+          </Col>
+          <Col className="d-flex align-items-center justify-content-center">
+          <Button variant="primary" type="button" size="lg">
+              CONNECT
+            </Button>
+          </Col>
+        </Row>
+      </Card>
+    </Container>
   );
 }
