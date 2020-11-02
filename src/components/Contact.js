c0230d382d8c1e8901d887ab068483ee0a97b0c6
Xavier Ritch <xritch89@hotmail.com>
2020-11-01 20:24:51 -0500

    made contact cards links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,14 +14,17 @@ export default function Contact() {
       </Card.Text>
       <Card.Body className="d-flex justify-content-around pt-0 w-75 mx-auto">
         <Card text="dark" className="connect-icon github three-d">
+            <a href='http://github.com/mrXavi3r' target='_blank' rel='noopener noreferrer' className='text-decoration-none text-dark'>
             <FontAwesomeIcon
               className="m-2 fa-github"
               icon={["fab", "github"]}
               size="3x"
             />
           <h6 className="small text-center">Github</h6>
+          </a>
         </Card>
         <Card text="dark" className="connect-icon mail three-d">
+        <a href='mailto:xritch89@hotmail.com' className='text-decoration-none text-dark'>
           <FontAwesomeIcon
             style={{ color: "#eb3349" }}
             className="m-2 fa-mail"
@@ -29,8 +32,10 @@ export default function Contact() {
             size="3x"
           />
           <h6 className="small text-center">Email</h6>
+          </a>
         </Card>
         <Card text="" className="connect-icon linkedin three-d">
+        <a href='https://www.linkedin.com/in/xavier-ritch-254238145/' target='_blank' rel='noopener noreferrer' className='text-decoration-none text-dark'>
           <FontAwesomeIcon
             style={{ color: "#115bdc" }}
             className="m-2 fa-linkedin"
@@ -38,8 +43,10 @@ export default function Contact() {
             size="3x"
           />
           <h6 className="small text-center">LinkedIn</h6>
+          </a>
         </Card>
         <Card text="" className="connect-icon phone three-d">
+        <a href="tel:123-456-7890" target='_blank' rel='noopener noreferrer' className='text-decoration-none text-dark'>
           <FontAwesomeIcon
             style={{ color: "#af52ec" }}
             className="m-2 fa-phone"
@@ -47,6 +54,7 @@ export default function Contact() {
             size="3x"
           />
           <h6 className="small text-center">Phone</h6>
+          </a>
         </Card>
       </Card.Body>
     </Card>
